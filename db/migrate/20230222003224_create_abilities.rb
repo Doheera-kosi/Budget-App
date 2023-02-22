d0b95839696bff780441993b542016b80f6173b6
Doheera-kosi <kupourevans99@gmail.com>
2023-02-22 00:48:24 +0000

    Add Migrations

--- db/migrate/20230222003224_create_abilities.rb
+++
@@ -1,8 +0,0 @@
-class CreateAbilities < ActiveRecord::Migration[7.0]
-  def change
-    create_table :abilities do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20230222003533_create_dealings.rb
+++
@@ -1,8 +0,0 @@
-class CreateDealings < ActiveRecord::Migration[7.0]
-  def change
-    create_table :dealings do |t|
-
-      t.timestamps
-    end
-  end
-end
